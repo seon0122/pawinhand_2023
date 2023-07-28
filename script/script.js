ce817be167f54f6f3344b6860c34dcc7ec73fb1e
miseon <bunny62420@gmail.com>
2023-07-28 17:37:38 +0900

    swiper-slide

--- script/script.js
+++ script/script.js
@@ -27,3 +27,32 @@ close.addEventListener('click', function(){
     //모바일 메뉴 숨기기
     m_nav_open.style.display = 'none';
 })
+
+// main-swiper-slide
+// const 변수명 = new Swiper('적용대상');
+// const 변수명 = new Swiper('적용대상',{속성:값, 속성:값});
+const pawin_slide = new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:1000, //슬라이드 간격(밀리초) 기본3초
+        disableOnInteraction:false, //버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true, //마지막 슬라이드 -> 첫번째 슬라이드 자연스러운 변경
+    effect:'fade', //제자리 변경 슬라이드 효과
+    // direction:'vertical' //기본은 'horizontal'
+    navigation: { //이전, 다음 내비게이션 연결
+        // next, prev 객체 연결 시 부모를 안적으면
+        // body 안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        // 개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    },
+});
+
+const sub_slide = new Swiper('#sub_slide',{
+    autoplay:{delay:1500},
+    loop:true,
+    navigation: {
+        nextEl: '#sub_slide .swiper-button-next',
+        prevEl: '#sub_slide .swiper-button-prev',
+    },
+})

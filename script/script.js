5ea876dc402e96ad3f47c8dcf74b2703deb76ba5
miseon <bunny62420@gmail.com>
2023-08-01 18:03:53 +0900

    slide2

--- script/script.js
+++ script/script.js
@@ -57,3 +57,25 @@ const sub_slide = new Swiper('#sub_slide',{
         prevEl: '#sub_slide .swiper-button-prev',
     },
 })
+// pawin-slide2
+const pawin_slide2 = new Swiper('#pawin_slide2',{
+    autoplay:{delay:1000},
+    loop:true,
+    effect:'fade',
+});
+// 입양정보 slide
+const dog_info_slide = new Swiper('#dog_info_slide',{
+    slidesPerView:'auto', //breakpoints옵션추가시 'auto', 한번에 보이는 슬라이드 개수(예. 4)
+    spaceBetween:20, //슬라이드 사이 여백
+    autoplay:{delay:1000},
+    loop:true,
+    // swiper-slide 반응형웹 옵션
+    breakpoints:{
+        //해상도:{옵션:값}
+        //해상도 순서는 작은 해상도 -> 큰 해상도 순으로 작성
+        400:{slidesPerView:2},
+        // 700:{slidesPerView:2}, //700이상일 때
+        900:{slidesPerView:3}, //900이상일 때
+        1160:{slidesPerView:4}, //1160이상일 경우 슬라이드 4개 표시
+    }
+});
